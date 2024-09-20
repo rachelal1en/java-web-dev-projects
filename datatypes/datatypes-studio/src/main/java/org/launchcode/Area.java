5018dcac87872cc9898712efc54d93e6f17c0524
Rachel Allen <rqllen@gmail.com>
2024-09-19 19:24:38 -0500

    RAA 19Sep24

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,20 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input;
+
+        input = new Scanner(System.in);
+        System.out.println("Enter a radius:");
+
+       double radius = input.nextDouble();
+        input.close();
+
+        double area = Circle.getArea(radius);
+
+        System.out.println("The area of a circle of radius " + radius + " is : " + area);
+    }
+
+    }
