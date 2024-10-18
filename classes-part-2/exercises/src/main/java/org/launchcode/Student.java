9dd748b3754a7e3ffde58687650064f7c395f599
Rachel Allen <rqllen@gmail.com>
2024-10-17 19:05:35 -0500

    RAA 17Oct24

--- classes-part-2/exercises/src/main/java/org/launchcode/Student.java
+++ classes-part-2/exercises/src/main/java/org/launchcode/Student.java
@@ -29,21 +29,45 @@ public class Student {
     }
 
 
-    //TODO: Uncomment and complete the getGradeLevel method here:
-//    public String getGradeLevel() {
-//        // Determine the grade level of the student based on numberOfCredits
-//    }
-
-    // TODO: Complete the addGrade method.
-    public void addGrade(int courseCredits, double grade) {
-        // Update the appropriate fields: numberOfCredits, gpa
+    public static String getGradeLevel(int credits) {
+        if (credits <= 29){
+            return "freshman";
+        } else if (credits <=59){
+            return "sophmore";
+        } else if (credits <= 89){
+            return "junior";
+        } else {
+            return "senior";
+        }
     }
 
-    // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather
-    //  than just the class fields.
+    public void addGrade(int courseCredits, double grade) {
+        double totalQuality = this.gpa * this.numberOfCredits;
+        totalQuality += grade * courseCredits;
+        this.numberOfCredits += courseCredits;
+        this.gpa = totalQuality/this.numberOfCredits;
+    }
 
-    // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
-    //  Student objects equal.
+    public String toString(){
+        String studentReport = String.format("%s is a %s with %d credits and a GPA of %.2f", this.name, this.getGradeLevel(this.numberOfCredits), this.getNumberOfCredits(), this.getGpa());
+        return studentReport;
+    }
+
+    public boolean equals(Object toBeCompared){
+        if (toBeCompared == this){
+            return true;
+        }
+        if (toBeCompared == null){
+            return false;
+        }
+
+        if (toBeCompared.getClass() != getClass()){
+            return false;
+        }
+
+        Student theStudent = (Student) toBeCompared;
+        return theStudent.getStudentId() == getStudentId();
+    }
 
     public String getName() {
         return name;
@@ -53,6 +77,9 @@ public class Student {
         return studentId;
     }
 
+    public double getGpa() {
+        return gpa;
+    }
     public int getNumberOfCredits() {
         return numberOfCredits;
     }
--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -7,7 +7,51 @@ import static org.junit.jupiter.api.Assertions.*;
 class BalancedBracketsTest {
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void nullReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+    @Test
+    public void noBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("   000"));
+    }
+    @Test
+    public void wrongTypeofBracketReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("}"));
+    }
+    @Test
+    public void textWithOutsideBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+    @Test
+    public void textWithInsideBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+    @Test
+    public void textWithEdgeBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+    @Test
+    public void backwardBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void backwardBracketsWithTextReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+    @Test
+    public void onlyOneBracketWithTextReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+    @Test
+    public void onlyOneBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void oddNumberOfBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[][]["));
     }
 }
